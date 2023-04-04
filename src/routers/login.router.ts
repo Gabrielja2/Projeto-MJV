dd49e1d9b45ce2ba39a1dfc07715accd2069e30d
Gabriel <gabrielja2antunes@gmail.com>
2023-04-04 14:13:54 -0300

    🚀 features: create login.router

---
+++ src/routers/login.router.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import UserController from '../controllers/user.controller';
+import UserService from '../services/user.service';
+import loginMiddleware from '../middlewares/login.middleware';
+
+const loginRouter = Router();
+const userService = new UserService();
+const userController = new UserController(userService);
+
+
+loginRouter.post('/', loginMiddleware, userController.login);
+
+
+export default loginRouter;
