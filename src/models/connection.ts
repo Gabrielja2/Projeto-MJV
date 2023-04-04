124cd007948df4245894a2d95c2678accf5dda1c
Gabriel <gabrielja2antunes@gmail.com>
2023-04-04 14:14:59 -0300

    🚚 build files: connection

---
+++ src/models/connection.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+const MONGO_DB_URL = 'mongodb://localhost:27017/next-todo';
+
+const connectToDatabase = (
+  mongoDatabaseURI = process.env.MONGO_URL || MONGO_DB_URL,
+) => mongoose.connect(mongoDatabaseURI);
+
+export default connectToDatabase;
