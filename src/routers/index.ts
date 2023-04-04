dcc79eb7af2b6e584a8d03877acb622e3d4f401e
Gabriel <gabrielja2antunes@gmail.com>
2023-04-04 14:14:14 -0300

    🚀 features: create register.router

---
+++ src/routers/index.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import healthRouter from "./health.router";
+import loginRouter from "./login.router";
+import registerRouter from "./register.router";
+import juiceRouter from "./juice.router"
+import userRouter from "./user.router";
+import authMiddleware from '../middlewares/auth.middleware';
+
+
+const router = Router();
+router.use('/health', healthRouter);
+router.use('/login', loginRouter);
+router.use('/register', registerRouter);
+router.use('/user', authMiddleware, userRouter)
+router.use('/juice', authMiddleware, juiceRouter);
+
+
+export default router;
