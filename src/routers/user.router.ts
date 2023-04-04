fa859e45fe8b179131ae071864c3ab6cb9f5e93c
Gabriel <gabrielja2antunes@gmail.com>
2023-04-04 14:22:06 -0300

    🚀 features: create user.router

---
+++ src/routers/user.router.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import UserController from '../controllers/user.controller';
+import UserService from '../services/user.service';
+import updateMiddleware from '../middlewares/update.user.middleware';
+
+const userRouter = Router();
+const userService = new UserService();
+const userController = new UserController(userService);
+
+userRouter.get('/', userController.show);
+userRouter.get('/:id', userController.showOne);
+userRouter.put('/:id', updateMiddleware, userController.update);
+userRouter.delete('/:id', userController.delete);
+
+export default userRouter;
