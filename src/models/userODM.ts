e7ae239890b7f1ce66728876dd16dd077c4b77a6
Gabriel <gabrielja2antunes@gmail.com>
2023-04-04 14:17:16 -0300

    🚀 features: create userODM

---
+++ src/models/userODM.ts
@@ -0,0 +1,27 @@
+import AbstractODM from './AbstractODM';
+import { IUser } from '../interfaces/user.interface';
+import { Schema } from 'mongoose';
+import { hash } from 'bcrypt';
+
+class UserODM extends AbstractODM<IUser> {
+  constructor() {
+    const schema = new Schema({
+      email: { type: String, required: true },
+      password: { type: String, required: true },
+      username: { type: String, required: true }
+    }, { versionKey: false });
+
+    schema.pre('save', async function (next) {
+
+      if (!this.isModified('password')) return next();
+
+      const hashedPassword = await hash(this.password, 10);
+      this.password = hashedPassword;
+      next();
+    });
+
+    super(schema, 'User');
+  }
+}
+
+export default UserODM;
