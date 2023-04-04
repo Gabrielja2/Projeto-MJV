c98cfdec66c1ebf58b3600d2eadf2fd280b4c6fe
Gabriel <gabrielja2antunes@gmail.com>
2023-04-04 19:20:39 -0300

    🛠️ refactoring: register router

--- src/routers/register.router.ts
+++ src/routers/register.router.ts
@@ -1,12 +1,12 @@
-import { Router } from 'express'
-import UserController from '../controllers/user.controller'
-import UserService from '../services/user.service'
-import registerMiddleware from '../middlewares/register.middleware'
+// import { Router } from 'express'
+// import UserController from '../controllers/user.controller'
+// import UserService from '../services/user.service'
+// import registerMiddleware from '../middlewares/register.middleware'
 
-const registerRouter = Router()
-const userService = new UserService()
-const userController = new UserController(userService)
+// const registerRouter = Router()
+// const userService = new UserService()
+// const userController = new UserController(userService)
 
-registerRouter.post('/', registerMiddleware, userController.create)
+// registerRouter.post('/', registerMiddleware, userController.create)
 
-export default registerRouter
+// export default registerRouter
