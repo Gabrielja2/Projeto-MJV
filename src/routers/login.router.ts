8ed87ee85f62f804f307c949a89d8093dcac891d
Gabriel <gabrielja2antunes@gmail.com>
2023-04-04 19:20:23 -0300

    🛠️ refactoring: login router

--- src/routers/login.router.ts
+++ src/routers/login.router.ts
@@ -1,12 +1,12 @@
-import { Router } from 'express'
-import UserController from '../controllers/user.controller'
-import UserService from '../services/user.service'
-import loginMiddleware from '../middlewares/login.middleware'
+// import { Router } from 'express'
+// import UserController from '../controllers/user.controller'
+// import UserService from '../services/user.service'
+// import loginMiddleware from '../middlewares/login.middleware'
 
-const loginRouter = Router()
-const userService = new UserService()
-const userController = new UserController(userService)
+// const loginRouter = Router()
+// const userService = new UserService()
+// const userController = new UserController(userService)
 
-loginRouter.post('/', loginMiddleware, userController.login)
+// loginRouter.post('/', loginMiddleware, userController.login)
 
-export default loginRouter
+// export default loginRouter
