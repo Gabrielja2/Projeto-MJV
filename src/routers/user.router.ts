90a37ae1d9791581811bb147e945ececaecc6e77
Gabriel <gabrielja2antunes@gmail.com>
2023-04-04 19:20:33 -0300

    🛠️ refactoring: user router

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -2,14 +2,19 @@ import { Router } from 'express'
 import UserController from '../controllers/user.controller'
 import UserService from '../services/user.service'
 import updateUserMiddleware from '../middlewares/update.user.middleware'
+import loginMiddleware from '../middlewares/login.middleware'
+import authMiddleware from '../middlewares/auth.middleware'
+import registerMiddleware from '../middlewares/register.middleware'
 
 const userRouter = Router()
 const userService = new UserService()
 const userController = new UserController(userService)
 
-userRouter.get('/', userController.show)
-userRouter.get('/:id', userController.showOne)
-userRouter.put('/:id', updateUserMiddleware, userController.update)
-userRouter.delete('/:id', userController.delete)
+userRouter.post('/login', loginMiddleware, userController.login)
+userRouter.post('/register', registerMiddleware, userController.create)
+userRouter.get('/', authMiddleware, userController.show)
+userRouter.get('/:id', authMiddleware, userController.showOne)
+userRouter.put('/:id', authMiddleware, updateUserMiddleware, userController.update)
+userRouter.delete('/:id', authMiddleware, userController.delete)
 
 export default userRouter
