199ad7b6f8f0c05ddee6ac0c3f058ccec3891aa6
Gabriel <gabrielja2antunes@gmail.com>
2023-04-05 13:30:07 -0300

    🚀 features: create juice.router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import healthRouter from './health.router'
 import juiceRouter from './juice.router'
 import userRouter from './user.router'
+// import orderRouter from './order.router'
 import authMiddleware from '../middlewares/auth.middleware'
 
 const router = Router()
 router.use('/health', healthRouter)
 router.use('/user', userRouter)
 router.use('/juice', authMiddleware, juiceRouter)
+// router.use('/order', authMiddleware, orderRouter)
 
 export default router
---
+++ src/routers/juice.router.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import JuiceController from '../controllers/juice.controller'
+import JuiceService from '../services/juice.service'
+import juiceMiddleware from '../middlewares/juice.middleware'
+
+const juiceRouter = Router()
+const juiceService = new JuiceService()
+const juiceController = new JuiceController(juiceService)
+
+juiceRouter.get('/', juiceController.show)
+juiceRouter.get('/:id', juiceController.showOne)
+juiceRouter.delete('/:id', juiceController.delete)
+juiceRouter.post('/', juiceMiddleware, juiceController.create)
+juiceRouter.put('/:id', juiceMiddleware, juiceController.update)
+
+export default juiceRouter
