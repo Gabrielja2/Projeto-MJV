198f3bd24c6e85cae0c2dd0b3886e7c5a4358159
Gabriel <gabrielja2antunes@gmail.com>
2023-04-06 11:55:54 -0300

    🚀 features: create order validations

---
+++ src/validations/order.schema.ts
@@ -0,0 +1,24 @@
+import Joi from 'joi'
+
+const orderSchema = Joi.object({
+  flavor: Joi.string().min(3).required().messages({
+    'any.required': 'Flavor field is required',
+    'string.empty': 'Flavor field can not be empty',
+    'string.min': 'Must have at least 3 characters'
+
+  }),
+  quantity: Joi.number().required().integer().min(1).messages({
+    'any.required': 'Quantity field is required',
+    'number.base': 'Quantity must be a number',
+    'number.integer': 'Quantity must be an integer',
+    'number.min': 'Quantity must be at least 1'
+  }),
+  size: Joi.string().required().min(6).messages({
+    'any.required': 'Size field is required',
+    'string.empty': 'Size field can not be empty',
+    'string.min': 'Must have at least 6 characters'
+
+  })
+})
+
+export default orderSchema
